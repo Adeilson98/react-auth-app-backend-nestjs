a1712e652949bcbc7a992b3d39c3c5bdd8f074f9
Adeilson Silva <adeilson.129@gmail.com>
2025-05-29 08:57:08 -0300

    modulo de usuario pronto

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(User)
+        private usersRepository: Repository<User>
+    ) {}
+
+    async findOneByUsername(username: string): Promise< User | null> {
+        return this.usersRepository.findOne({where: {username}});
+    }
+
+    async createUser(username: string, passwordHash: string): Promise<User> {
+        const newUser = this.usersRepository.create({username, password: passwordHash});
+        return this.usersRepository.save(newUser);
+    }
+
+    async findUserById(id: number): Promise<User | null> {
+        return this.usersRepository.findOne({where: {id}});
+    }
+}
