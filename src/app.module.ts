0cac556cb746f0a5c69782ed5efe584cfc050ce4
Adeilson Silva <adeilson.129@gmail.com>
2025-05-31 21:16:18 -0300

    ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,36 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { ProductModule } from './product/product.module';
-import { AuthModule } from './nest/auth/auth.module';
-import { AuthModule } from './auth/auth.module';
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { UserModule } from './user/user.module';
+// import { ProductModule } from './product/product.module';
+// import { AuthModule } from './auth/auth.module';
+
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ProductModule } from "./product/product.module";
+import { AuthModule } from "./auth/auth.module";
+import { UserModule } from "./user/user.module";
+import { User } from "./user/user.entity";
+import { Product } from "./product/product.entity";
 
 @Module({
-  imports: [UserModule, ProductModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root', // Substitua pelo seu usuário
+      password: '',   // Substitua pela sua senha
+      database: 'react_auth_products_db',
+      entities: [User, Product], // Registre TODAS as suas entidades aqui
+      synchronize: true, // CUIDADO: Use `migrations` em produção!
+    }),
+    UserModule, 
+    ProductModule, 
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, HttpCode, HttpStatus, UseGuards, Req } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { AuthGuard } from '@nestjs/passport';
 
-@Controller('product')
-export class ProductController {}
+@Controller('api/products')
+export class ProductController {
+    constructor(private readonly productService: ProductService) {}
+
+    @UseGuards(AuthGuard('jwt'))
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    async create(@Body() createProductDto: CreateProductDto, @Req() req) {
+        const userId = req.user.userId;
+        return this.productService.create(createProductDto, userId);
+    }
+
+    @UseGuards(AuthGuard('jwt'))
+    @Get()
+    async findAll(@Req() req) {
+        const userId = req.user.userId;
+        return this.productService.findAllByUserId(userId);
+    }
+}
